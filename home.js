ab7959ab2962a4948cd13a51a63c97590c2e2ab0
hatem <info@hatemconsulting.com>
2024-07-13 18:20:31 +0300

    depi js lap3

---
+++ home.js
@@ -0,0 +1,36 @@
+function isDivBy2 () {
+    let inputValue = document.getElementById("input").value
+    if (inputValue % 2) {
+        console.log("yes" , inputValue , "is divisible by 2")
+    } else {
+        console.log ("no" , inputValue , "is not div by 2")
+    }
+}
+function isDivBy3 () {
+    let inputValue = document.getElementById("input").value
+    if (inputValue % 3) {
+        console.log("yes" , inputValue , "is divisible by 3")
+    } else {
+        console.log ("no" , inputValue , "is not div by 3")
+    }
+}
+function isDivBy5 () {
+    let inputValue = document.getElementById("input").value
+    if (inputValue % 5) {
+        console.log("yes" , inputValue , "is divisible by 5")
+    } else {
+        console.log ("no" , inputValue , "is not div by 5")
+    }
+}
+function isDivBy7 () {
+    let inputValue = document.getElementById("input").value
+    if (inputValue % 7) {
+        console.log("yes" , inputValue , "is divisible by 7")
+    } else {
+        console.log ("no" , inputValue , "is not div by 7")
+    }
+}
+
+function assertion () {
+    if (inputValue)
+}
